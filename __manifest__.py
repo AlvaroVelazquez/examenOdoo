002ee0f5e4b242dd7cc534b25e253dfd92ca44b3
parktem <alejandromayo98@gmail.com>
2019-02-18 17:47:18 +0100

    Estructura inicial hecha

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "taller",
+
+    'summary': """
+        modulo base""",
+
+    'description': """
+        Modulo para aprender (taller)
+    """,
+
+    'author': "Alberto Pasamar, Alejandro Lahoz, Hector Lagier, Alvaro Velázquez, Javier Inés, Ignacio Lou",
+    'website': "https://zaragoza.salesianos.edu/",
+    'category': 'trabajo',
+    'version': '1.0',
+    'depends': ['base', 'baseModule'],
+    'data': [
+        'views/views.xml',
+    ],
+}
