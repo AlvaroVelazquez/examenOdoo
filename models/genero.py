397dfffc2ea5f68356ec10ad2a7c7801d84ab258
AlvaroVelazquez <alvarovelazquez12345@gmail.com>
2019-02-25 08:56:58 +0100

    segundo commit

--- models/genero.py
+++ models/genero.py
@@ -3,9 +3,7 @@ from odoo import models,fields, api
 class genero(models.Model):
     
     name = fields.Char(string="name", required=True, help="Nombre del genero")
-    direccion = fields.Char(string="direccion", help="Nombre de la calle")
-    coche_ids = fields.One2many("res.partner", "taller_id", string="coches")
-    mecanico_ids = fields.One2many("base.entidad", "taller_id", string="mecanicos")
+    videojuegos_ids = fields.One2many("genero_id", string="videojuegos")
 
 
 
